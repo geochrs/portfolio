7456b44141806c4a0c0916cba41a9269018db5fd
geochrs <xgogos@hotmail.com>
2023-08-08 16:32:29 +0300

    initial files

---
+++ app.js
@@ -0,0 +1,54 @@
+const navbar = document.querySelector('.navBar');
+window.addEventListener('scroll', function () {
+   if (document.documentElement.scrollTop > 20) {
+      navbar.classList.add('sticky');
+   } else {
+      navbar.classList.remove('sticky');
+   }
+});
+
+const homeBtn = document.querySelectorAll('.nav-buttons')[0];
+homeBtn.addEventListener('click', () => {
+   document.documentElement.scrollTop = 0;
+});
+
+const about = {
+   btn: document.querySelectorAll('.nav-buttons')[1],
+   tag: document.querySelector('#aboutNav')
+};
+
+const work = {
+   btn: document.querySelectorAll('.nav-buttons')[2],
+   tag: document.querySelector('#workNav')
+};
+
+const contact = {
+   btn: document.querySelectorAll('.nav-buttons')[3],
+   tag: document.querySelector('#contactNav')
+}
+
+function scrollTo(el) {
+   el.tag.scrollIntoView();
+}
+
+about.btn.addEventListener('click', function () {
+   scrollTo(about);
+})
+
+work.btn.addEventListener('click', function () {
+   scrollTo(work);
+})
+
+contact.btn.addEventListener('click', function () {
+   scrollTo(contact);
+})
+
+const buttons = document.querySelectorAll('.nav-buttons');
+buttons.forEach(button => {
+   button.addEventListener('click', function () {
+      buttons.forEach(btn => btn.classList.remove('selected'));
+      this.classList.add('selected');
+   });
+});
+
+ScrollReveal().reveal('.headline', { delay: 400 });
