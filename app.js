300e96c771f899f382c214d202224fa5def95975
geochrs <geo.christopoulos@hotmail.com>
2024-12-11 10:35:44 +0200

    changed navbar box-shadow and color to show only when scrolling

--- app.js
+++ app.js
@@ -3,55 +3,61 @@ const navbar = document.querySelector('.navBar');
 let prevScrollPos = window.scrollY;
 
 window.addEventListener('scroll', function () {
-   let currScrollPos = window.scrollY;
-   if (currScrollPos > prevScrollPos) {
-      navbar.style.transform = `translateY(-105%)`;
-   } else {
-      navbar.style.transform = `translateY(0%)`;
-   }
-   prevScrollPos = currScrollPos;
-})
+  let currScrollPos = window.scrollY;
+  if (currScrollPos === 0) {
+    navbar.style.boxShadow = '';
+    navbar.style.backgroundColor = '';
+    navbar.style.transform = `translateY(0%)`;
+  } else if (currScrollPos > prevScrollPos) {
+    navbar.style.transform = `translateY(-105%)`;
+  } else {
+    navbar.style.transform = `translateY(0%)`;
+    navbar.style.boxShadow = `0 2px 4px rgba(0, 0, 0, 0.1)`;
+    navbar.style.backgroundColor = '#f8f9fa';
+  }
+  prevScrollPos = currScrollPos;
+});
 
 //add selected while scrolling
-const sections = document.querySelectorAll("section[id]");
-window.addEventListener("scroll", activeLink);
+const sections = document.querySelectorAll('section[id]');
+window.addEventListener('scroll', activeLink);
 
 function activeLink() {
+  let scrollY = window.scrollY;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 200;
+    let sectionId = current.getAttribute('id');
 
-   let scrollY = window.scrollY;
-   sections.forEach(current => {
-      const sectionHeight = current.offsetHeight;
-      const sectionTop = current.offsetTop - 200;
-      let sectionId = current.getAttribute("id");
-
-      if (
-         scrollY > sectionTop &&
-         scrollY <= sectionTop + sectionHeight
-      ) {
-         document.querySelector(".nav-container a[href*=" + sectionId + "]").classList.add("selected");
-      } else {
-         document.querySelector(".nav-container a[href*=" + sectionId + "]").classList.remove("selected");
-      }
-   });
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav-container a[href*=' + sectionId + ']')
+        .classList.add('selected');
+    } else {
+      document
+        .querySelector('.nav-container a[href*=' + sectionId + ']')
+        .classList.remove('selected');
+    }
+  });
 }
 
 //hamburger menu when mobile
-const hamburger = document.querySelector(".hamburger");
-const navContainer = document.querySelector(".nav-container");
+const hamburger = document.querySelector('.hamburger');
+const navContainer = document.querySelector('.nav-container');
 const navLink = document.querySelectorAll('.nav-links');
 
 function mobileMenu() {
-   hamburger.classList.toggle("active");
-   navContainer.classList.toggle("active");
+  hamburger.classList.toggle('active');
+  navContainer.classList.toggle('active');
 }
 
 function closeMenu() {
-   hamburger.classList.remove('active');
-   navContainer.classList.remove('active');
+  hamburger.classList.remove('active');
+  navContainer.classList.remove('active');
 }
 
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener('click', closeMenu));
+hamburger.addEventListener('click', mobileMenu);
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
 //send email
 const inputs = document.querySelectorAll('input');
@@ -61,32 +67,34 @@ const alertbox = document.querySelector('.alert');
 const closebtn = document.querySelector('.closebtn');
 
 function sendMail() {
-   let params = {
-      from_name: document.querySelector('.person-name').value,
-      email_id: document.querySelector('.person-email').value,
-      message: document.querySelector('.person-text').value
-   }
-   emailjs.send('service_9ecsisg', 'template_c4nafhh', params).then(function (res) {
+  let params = {
+    from_name: document.querySelector('.person-name').value,
+    email_id: document.querySelector('.person-email').value,
+    message: document.querySelector('.person-text').value,
+  };
+  emailjs
+    .send('service_9ecsisg', 'template_c4nafhh', params)
+    .then(function (res) {
       alertbox.classList.add('alertOn');
-   })
+    });
 }
 
 form.addEventListener('click', function (e) {
-   if (form.checkValidity()) {
-      e.preventDefault();
-      sendMail();
-      inputs.forEach(input => input.value = '');
-      textarea.value = '';
-   }
-})
+  if (form.checkValidity()) {
+    e.preventDefault();
+    sendMail();
+    inputs.forEach((input) => (input.value = ''));
+    textarea.value = '';
+  }
+});
 
 closebtn.addEventListener('click', function () {
-   this.parentElement.classList.remove('alertOn');
-})
+  this.parentElement.classList.remove('alertOn');
+});
 
 //typing effect
-let typingEffect = new TypeIt(".typedText", {
-   strings: "George Christopoulos.",
-   speed: 120,
-   waitUntilVisible: true,
+let typingEffect = new TypeIt('.typedText', {
+  strings: 'George Christopoulos.',
+  speed: 120,
+  waitUntilVisible: true,
 }).go();
